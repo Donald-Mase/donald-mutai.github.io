6448dbf7334826b00ce87bfecb003e3531a100df
Donald-Mase <donald-kipruto@proton.me>
2023-09-29 15:11:24 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { Component } from "react";
+import ReactGA from "react-ga";
+import $ from "jquery";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import Resume from "./Components/Resume";
+import Contact from "./Components/Contact";
+import Portfolio from "./Components/Portfolio";
 
-function App() {
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      foo: "bar",
+      resumeData: {}
+    };
+
+    ReactGA.initialize("UA-110570651-1");
+    ReactGA.pageview(window.location.pathname);
+  }
+
+  getResumeData() {
+    $.ajax({
+      url: "./resumeData.json",
+      dataType: "json",
+      cache: false,
+      success: function(data) {
+        this.setState({ resumeData: data });
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.log(err);
+        alert(err);
+      }
+    });
+  }
+
+  componentDidMount() {
+    this.getResumeData();
+  }
+
+  render() {
   return (
     <div className="App">
+    <Header data={this.state.resumeData.main} />
+    <Resume data={this.state.resumeData.resume} />
+    <Portfolio data={this.state.resumeData.portfolio} />
+    <Contact data={this.state.resumeData.main} />
+    <Footer data={this.state.resumeData.main} />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -18,8 +62,9 @@ function App() {
           Learn React
         </a>
       </header>
+      <Footer data={this.state.resumeData.main} />
     </div>
   );
 }
-
+}
 export default App;
